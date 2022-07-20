2ae1f74d37617fe38f594cef34927c82f86e673c
psichoxis <poli.cs@hotmail.com>
2022-07-19 23:17:51 -0300

    Segundo desafio

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+      <div>
+        <a href="">Galactic Cakes</a>
+        <div>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">¿Quienes Somos?</a>
+            </li>
+            <li>
+              <a href="">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+  )
+}
+
+export default NavBar
