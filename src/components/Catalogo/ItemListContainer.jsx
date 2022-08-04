b3fe2bbc1a9682d4f01740dfb5bab0cef41dda69
psichoxis <poli.cs@hotmail.com>
2022-08-04 12:56:48 -0300

    Quinto desafio correccion

--- src/components/Catalogo/ItemListContainer.jsx
+++ src/components/Catalogo/ItemListContainer.jsx
@@ -5,94 +5,101 @@ const ItemListContainer = () => {
     const [items,setItems]=useState([])
 
     useEffect(()=>{
-      setTimeout(()=>{
-        setItems([
-          {
-            "id": 1,
-            "precio": 500,
-            "stock": 5,
-            "title": "Tiramisú",
-            "url": "../../img/tiramisu.jpeg"
-          },
-          {
-            "id": 2,
-            "precio": 550,
-            "stock": 5,
-            "title": "Choco oreo",
-            "url": "../../img/chocooreo.jpeg"
-          },
-          {
-            "id": 3,
-            "precio": 500,
-            "stock": 5,
-            "title": "Brownie",
-            "url": "../../img/brownie.jpeg"
-          },
-          {
-            "id": 4,
-            "precio": 550,
-            "stock": 5,
-            "title": "Oreo",
-            "url": "../../img/oreo.jpeg"
-          },
-          {
-            "id": 5,
-            "precio": 500,
-            "stock": 5,
-            "title": "Chocoflan",
-            "url": "../../img/chocoflan.jpeg"
-          },
-          {
-            "id": 6,
-            "precio": 500,
-            "stock": 5,
-            "title": "Chocotorta",
-            "url": "../../img/chocotorta.jpeg"
-          },
-          {
-            "id": 7,
-            "precio": 500,
-            "stock": 5,
-            "title": "Crema Moka",
-            "url": "../../img/moka.jpeg"
-          },
-          {
-            "id": 8,
-            "precio": 500,
-            "stock": 5,
-            "title": "Lemon Pie",
-            "url": "../../img/lemonpie.jpeg"
-          },
-          {
-            "id": 9,
-            "precio": 500,
-            "stock": 5,
-            "title": "Lemon Brownie",
-            "url": "../../img/lemonbrownie.jpeg"
-          },
-          {
-            "id": 10,
-            "precio": 700,
-            "stock": 5,
-            "title": "Budín de Mandarina",
-            "url": "../../img/budin.jpeg"
-          },
-          {
-            "id": 11,
-            "precio": 250,
-            "stock": 5,
-            "title": "Alfajor de Maicena",
-            "url": "../../img/alfajormaicena.jpeg"
-          },
-          {
-            "id": 12,
-            "precio": 250,
-            "stock": 5,
-            "title": "Alfajor de Chocolate",
-            "url": "../../img/alfajorchoco.jpeg"
-          }]
-        )
-      }, 2000);
+      const miPromesa = new Promise((res, rej) => {
+        setTimeout(() => {
+          res(
+            [{
+                "id": 1,
+                "precio": 500,
+                "stock": 5,
+                "title": "Tiramisú",
+                "url": "../../img/tiramisu.jpeg"
+              },
+              {
+                "id": 2,
+                "precio": 550,
+                "stock": 5,
+                "title": "Choco oreo",
+                "url": "../../img/chocooreo.jpeg"
+              },
+              {
+                "id": 3,
+                "precio": 500,
+                "stock": 5,
+                "title": "Brownie",
+                "url": "../../img/brownie.jpeg"
+              },
+              {
+                "id": 4,
+                "precio": 550,
+                "stock": 5,
+                "title": "Oreo",
+                "url": "../../img/oreo.jpeg"
+              },
+              {
+                "id": 5,
+                "precio": 500,
+                "stock": 5,
+                "title": "Chocoflan",
+                "url": "../../img/chocoflan.jpeg"
+              },
+              {
+                "id": 6,
+                "precio": 500,
+                "stock": 5,
+                "title": "Chocotorta",
+                "url": "../../img/chocotorta.jpeg"
+              },
+              {
+                "id": 7,
+                "precio": 500,
+                "stock": 5,
+                "title": "Crema Moka",
+                "url": "../../img/moka.jpeg"
+              },
+              {
+                "id": 8,
+                "precio": 500,
+                "stock": 5,
+                "title": "Lemon Pie",
+                "url": "../../img/lemonpie.jpeg"
+              },
+              {
+                "id": 9,
+                "precio": 500,
+                "stock": 5,
+                "title": "Lemon Brownie",
+                "url": "../../img/lemonbrownie.jpeg"
+              },
+              {
+                "id": 10,
+                "precio": 700,
+                "stock": 5,
+                "title": "Budín de Mandarina",
+                "url": "../../img/budin.jpeg"
+              },
+              {
+                "id": 11,
+                "precio": 250,
+                "stock": 5,
+                "title": "Alfajor de Maicena",
+                "url": "../../img/alfajormaicena.jpeg"
+              },
+              {
+                "id": 12,
+                "precio": 250,
+                "stock": 5,
+                "title": "Alfajor de Chocolate",
+                "url": "../../img/alfajorchoco.jpeg"
+              }]
+          );
+        }, 2000);
+      });
+  
+      miPromesa.then((res) => {
+        setItems(res);
+      });
+
       },[])
     
 
